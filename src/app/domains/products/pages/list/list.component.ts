31886e7b722a7693991ee3dd7ae5bdfe444dab92
sergio <sergioalejandrogutierrezmedina@gmail.com>
2024-07-19 21:24:50 -0500

    updates files

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+    fromChild(event: string) {
+      console.log('estamos en el padre');
+      console.log(event);
+    }
+}
