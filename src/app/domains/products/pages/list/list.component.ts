e34fab7f741055f799eaaea39b3f3a7b7ac0f450
sergio <sergioalejandrogutierrezmedina@gmail.com>
2024-07-25 21:02:15 -0500

    Addition of components

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { ProductComponent } from '../../components/product/product.component';
 import { CommonModule } from '@angular/common';
 
+import { ProductComponent } from '../../components/product/product.component';
 
 @Component({
   selector: 'app-list',
