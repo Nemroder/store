fbb782f066e2dfb14a1b3a648ae1564a1c67a6c1
sergio <sergioalejandrogutierrezmedina@gmail.com>
2024-07-31 11:33:40 -0500

    counter changes

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges,  } from '@angular/core';
+import { Component, Input, signal, SimpleChanges,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
